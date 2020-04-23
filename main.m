11ef7548b506dad12f96e21cb2681b0fb7ed777a
Shashank Ojha <shashanko@gmail.com>
2020-04-23 18:33:02 -0400

    Main file

---
+++ main.m
@@ -0,0 +1,35 @@
+
+
+% Nasic information about our images 
+
+width = 640;
+height = 480;
+num_images = 1362;
+
+% CALIBRATION OF THE COLOR CAMERA (https://vision.in.tum.de/data/datasets/rgbd-dataset/file_formats#intrinsic_camera_calibration_of_the_kinect)
+fx = 517.3;
+fy = 516.5;
+cx = 318.6;
+cy = 255.3;
+
+d0 = 0.2624;
+d1 = -0.9531;
+d2 = -0.0054;
+d3 = 0.0026;
+d4 = 1.1633;
+
+% CALIBRATION OF THE DEPTH IMAGES (https://vision.in.tum.de/data/datasets/rgbd-dataset/file_formats#intrinsic_camera_calibration_of_the_kinect
+ds = 1.035;
+
+ss
+% TODO: Load all data into memory (Note: read associations.txt file to see which
+%       rgb file corresponds to which depth image)
+
+
+% TODO: back_projection
+
+
+% TDOD: fusion
+
+
+
